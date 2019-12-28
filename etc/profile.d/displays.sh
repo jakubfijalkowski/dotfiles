b644ca70c4fa2abb54bca4fc90f7f7c5013524f0
Jakub Fijałkowski <kuba@codinginfinity.me>
2019-12-28 17:33:03 +0100

    Remove bashisms from scripts and switch to `sh` where appropriate

--- etc/profile.d/displays.sh
+++ etc/profile.d/displays.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env zsh
+#!/bin/sh
 
-if [[ $(xrandr | grep 'DP-1-1') ]]; then
+if [ "$(xrandr | grep 'DP-1-1')" != "" ]; then
     # Nvidia
-    typeset -g DISPLAY_CONFIG_PRIMARY='DP-1-1'
-    typeset -g DISPLAY_CONFIG_SECONDARY='eDP-1-1'
+    DISPLAY_CONFIG_PRIMARY='DP-1-1'
+    DISPLAY_CONFIG_SECONDARY='eDP-1-1'
 else
     # Intel (modesetting)
-    typeset -g DISPLAY_CONFIG_PRIMARY='DP-1'
-    typeset -g DISPLAY_CONFIG_SECONDARY='eDP-1'
+    DISPLAY_CONFIG_PRIMARY='DP-1'
+    DISPLAY_CONFIG_SECONDARY='eDP-1'
 fi
 
-sed -i "s/screens.primary:.\+/screens.primary: $DISPLAY_CONFIG_PRIMARY/" .Xresources
-sed -i "s/screens.secondary:.\+/screens.secondary: $DISPLAY_CONFIG_SECONDARY/" .Xresources
+sed -i "s/screens.primary:.\+/screens.primary: $DISPLAY_CONFIG_PRIMARY/" $HOME/.Xresources
+sed -i "s/screens.secondary:.\+/screens.secondary: $DISPLAY_CONFIG_SECONDARY/" $HOME/.Xresources
+xrdb $HOME/.Xresources
