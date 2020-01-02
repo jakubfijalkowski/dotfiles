97fa20b60c33fba2450f461135595bdc2b246386
Jakub Fijałkowski <kuba@codinginfinity.me>
2020-01-02 10:08:31 +0100

    Detect HDMI-based systems in `displays.sh`

--- etc/profile.d/displays.sh
+++ etc/profile.d/displays.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-if [ "$(xrandr | grep 'DP-1-1')" != "" ]; then
+if [ "$(xrandr | grep 'DP-1-1 connected')" != "" ]; then
     # Nvidia
     DISPLAY_CONFIG_PRIMARY='DP-1-1'
     DISPLAY_CONFIG_SECONDARY='eDP-1-1'
-else
+elif [ "$(xrandr | grep 'DP-1 connected')" != "" ]; then
     # Intel (modesetting)
     DISPLAY_CONFIG_PRIMARY='DP-1'
     DISPLAY_CONFIG_SECONDARY='eDP-1'
+else
+    # Otherwise - HDMI-based
+    DISPLAY_CONFIG_PRIMARY='HDMI-1'
+    DISPLAY_CONFIG_SECONDARY='HDMI-2'
 fi
 
 sed -i "s/screens.primary:.\+/screens.primary: $DISPLAY_CONFIG_PRIMARY/" $HOME/.Xresources
